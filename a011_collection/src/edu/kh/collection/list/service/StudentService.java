b71c2dc016b5cee4e08530ad0867729455c53e83
JaydenWork <kkangg92@gmail.com>
2023-02-28 14:01:51 +0900

    2023.02.28 점심시간 코드 공유용

--- a011_collection/src/edu/kh/collection/list/service/StudentService.java
+++ a011_collection/src/edu/kh/collection/list/service/StudentService.java
@@ -1,6 +1,7 @@
 package edu.kh.collection.list.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import edu.kh.collection.list.dto.Student;
@@ -8,68 +9,178 @@ import edu.kh.collection.list.dto.Student;
 public class StudentService {
 
 	private List<Student> studentList = new ArrayList<Student>();
-	
+
 	public StudentService() {
-		
+
 		studentList.add(new Student("정병종", 3, 5, 17, "서울시 중구 남대문로", 'M', 75));
 		studentList.add(new Student("정민영", 2, 7, 7, "서울시 성동구 아차산로", 'F', 85));
 		studentList.add(new Student("최대윤", 2, 1, 11, "서울시 도봉구 도봉로", 'F', 65));
 		studentList.add(new Student("김재창", 3, 9, 25, "서울시 강남구 청담동", 'M', 95));
 		studentList.add(new Student("이준규", 1, 3, 15, "제주도 서귀포시 청계로", 'M', 90));
-		
-		
+
 	}
-	
-	/** studentList에 학생 추가
+
+	/**
+	 * studentList에 학생 추가
 	 * 
 	 * @param std
 	 * @return true
 	 */
-	
+
 	public boolean addstudent(Student std) {
-		return studentList.add(std);  //add 매서드가 boolean트루를 반환한다(무조건)	
+		return studentList.add(std); // add 매서드가 boolean트루를 반환한다(무조건)
 	}
-	
-	/** 학생 전체 조회 서비스
+
+	/**
+	 * 학생 전체 조회 서비스
+	 * 
 	 * @return studentList
-	 * */
+	 */
 	public List<Student> selectAll() {
 		return studentList;
 	}
 
-	
-	/** 학생 정보 수정 서비스
+	/**
+	 * 학생 정보 수정 서비스
+	 * 
 	 * @param index
 	 * @param std
-	 * @return s : Student(수정되기 전 학생 정보)	
-	 * */
+	 * @return s : Student(수정되기 전 학생 정보)
+	 */
 	public Student updateStudent(int index, Student std) {
-		
-		//e2 set(int index, E e) : 1) index에 위치하는 요소를 e로 변경
-		// 						   2) 기존에 있던 요소 e2를 반환		
-		
-		
+
+		// e2 set(int index, E e) : 1) index에 위치하는 요소를 e로 변경
+		// 2) 기존에 있던 요소 e2를 반환
+
 		return studentList.set(index, std);
 	}
 
-	
-	/** 학생 정보 제거 서비스
+	/**
+	 * 학생 정보 제거 서비스
+	 * 
 	 * @param index
 	 * @return s: Student (제거된 학생 정보)
-	 * */
+	 */
 	public Student removeStudent(int index) {
-		
+
 		// E remove(int index) : index번째 요소를 List에서 제거하여 반환
-		
-		
+
 		// boolean remove(E e) : List에서 E와 일치하는 요소를 찾아서
-		// 						 있으면 제거하고 true
-		//						 없으면 false 반환
-		
-		
+		// 있으면 제거하고 true
+		// 없으면 false 반환
+
 		return studentList.remove(index);
-		
+
 	}
-	
-	
+
+	/**
+	 * 학생 이름 검색 서비스
+	 * 
+	 * @param name
+	 * @return list:List<Student> 이름이 일치하는 학생리스트
+	 */
+	public List<Student> selectName(String name) {
+
+		// 1) 검색 결과를 저장할 List<Student>생성
+		List<Student> list = new ArrayList<Student>();
+
+		// 2) studentList의 모든 요소를 순차 접근하면서 이름이 일치하는 학생을
+		// list에서 추가
+
+		for (Student s : studentList) { // s에 Student한명한명이 담긴다
+
+			if (s.getName().equals(name))
+				list.add(s);
+
+		}
+
+		// 3) 검색 결과 반환
+		return list;
+	}
+
+	/**
+	 * 학생 주소 검색 서비스
+	 * 
+	 * @param input
+	 * @return list:List<Student> 검색어가 주소에 포함된 학생 리스트
+	 */
+	public List<Student> selectAddress(String input) {
+
+		List<Student> list = new ArrayList<Student>();
+
+		for (Student s : studentList) { // s에 Student한명한명이 담긴다
+
+			// String.comtains("문자열) : String에 "문자열"이 포함되어 있으면 true 반환
+			if (s.getAddress().contains(input))
+				list.add(s);
+
+		}
+
+		return list;
+	}
+
+	public List<Student> selectGrade(int input) {
+
+		List<Student> list = new ArrayList<Student>();
+		// 제네릭의 타입 추론
+		// - 생성되는 컬렉션 객체의 제네릭을 별도 작성하지 않아도
+		// 참조변수의 제네릭을 통해 제한되는 타입을 유추(추론)
+
+		for (Student s : studentList) {
+			if (s.getGrade() == input)
+				list.add(s);
+		}
+		return list;
+	}
+
+	/**
+	 * 성별 조회 서비스
+	 * 
+	 * @param inputGender
+	 * @return list:List<Student> inputGender와 성별이 같은 학생 리스트
+	 */
+
+	public List<Student> selectGender(char inputGender) {
+		List<Student> list = new ArrayList<Student>();
+
+		for (Student s : studentList) {
+			if (s.getGender() == inputGender) {
+				list.add(s);
+			}
+		}
+
+		return list;
+	}
+
+	/**
+	 * 성적 순서 조회 서비스
+	 * 
+	 * @return studentList
+	 */
+	public List<Student> sortScore() {
+		
+		// studnetList 정렬(score 내림 차순)
+		// - Collections 클래스 : 컬렉션에 도움되는 유용한 기능을 모은 클래스
+
+		// - Comparable<T> 인터페이스
+		// -> 객체의 기본 정렬 기준을 제공하는 인터페이스
+
+		// <?> : 어떤게 작성될지 모름 == 아무거나 작성 가능
+		// <?> void Collections.sort(List<?> list)
+		
+		Collections.sort(studentList);
+		// studentList에 저장된 객체 Student의
+		// 오버라이딩된 compareTo() 메서드를 이용해서 정렬
+		// -> 현재 큰 숫자가 오른쪽으로 이동하도록 오버라이딩됨
+		// -> 오름차순 정렬
+
+		// void Collections.reverse(List<?> list)
+		// - list 순서를 반대로 뒤집음
+		// -> 오름차순 --> 내림차순
+	//Collections.reverse(studentList);
+		
+		return studentList;
+
+	}
+
 }
--- a011_collection/src/edu/kh/collection/list/view/StudentView.java
+++ a011_collection/src/edu/kh/collection/list/view/StudentView.java
@@ -9,15 +9,15 @@ import edu.kh.collection.list.service.StudentService;
 
 public class StudentView {
 
-	private Scanner sc = new Scanner(System.in);					
+	private Scanner sc = new Scanner(System.in);
 	private StudentService service = new StudentService(); // --->> [ 이 코드의 의미 ]
-	//																이 코드에서 private StudentService service = new StudentService();은
-	//																StudentView 클래스 내부에서 StudentService 클래스의 인스턴스를 생성하고, 
-	//																이를 service 필드에 할당하는 것을 의미합니다. 이렇게 함으로써 StudentView 클래스 내부에서 
-	//																StudentService 클래스의 메소드를 호출하여 학생 정보를 
-	//																추가, 조회, 수정, 삭제 등의 작업을 수행할 수 있게 됩니다. 
-	//																즉, StudentService 클래스의 기능을 StudentView 클래스에서 사용할 수 있도록 
-	//																인스턴스를 생성하고 참조하는 것입니다.
+	// 이 코드에서 private StudentService service = new StudentService();은
+	// StudentView 클래스 내부에서 StudentService 클래스의 인스턴스를 생성하고,
+	// 이를 service 필드에 할당하는 것을 의미합니다. 이렇게 함으로써 StudentView 클래스 내부에서
+	// StudentService 클래스의 메소드를 호출하여 학생 정보를
+	// 추가, 조회, 수정, 삭제 등의 작업을 수행할 수 있게 됩니다.
+	// 즉, StudentService 클래스의 기능을 StudentView 클래스에서 사용할 수 있도록
+	// 인스턴스를 생성하고 참조하는 것입니다.
 
 	public void displayMenu() {
 		int input = 0;
@@ -59,14 +59,19 @@ public class StudentView {
 					removeStudent();
 					break;
 				case 5:
+					selectName();
 					break;
 				case 6:
+					selectAddress();
 					break;
 				case 7:
+					selectGrade();
 					break;
 				case 8:
+					selectGender();
 					break;
 				case 9:
+					sortScore();
 					break;
 				case 0:
 					System.out.println("[ 프로그램 종료 ]");
@@ -91,10 +96,7 @@ public class StudentView {
 
 		// 학생 정보 입력 메서드 호출 후 결과 반환 받기
 		Student std = inputStudent();
-		
 
-		
-		
 		if (service.addstudent(std)) {
 			System.out.println("[ 학생 정보가 추가되었습니다. ]");
 		}
@@ -129,22 +131,19 @@ public class StudentView {
 		System.out.println("\n--- 학생 정보 수정 --- \n");
 
 		System.out.println("<수정할 학생 정보 입력> ");
-		
+
 		Student std = inputStudent();
-		
-		
+
 		System.out.println("---------------------------");
 		System.out.print("수정할 학생의 index : ");
 		int index = sc.nextInt();
-		
-		
+
 		Student s = service.updateStudent(index, std);
 		// s== 수정되기 전 학생 정보가 반환됨
-		
-		System.out.println(s.getName()+ "의 정보가 수정 되었습니다.");
-		
+
+		System.out.println(s.getName() + "의 정보가 수정 되었습니다.");
+
 	}
-	
 
 	/**
 	 * 학생 전체 정보를 입력 받아 Student 객체로 반환하는 메서드
@@ -179,17 +178,191 @@ public class StudentView {
 
 		return std;
 	}
-	
+
 	private void removeStudent() {
 		System.out.println("\n --- 학생 정보 제거 ---\n");
 		System.out.print("삭제할 학생의 인덱스 입력 : ");
-		
+
 		int index = sc.nextInt();
-		
-		
+
 		// ** 제거되기 전 학생 정보가 반환됨 **
 		Student s = service.removeStudent(index);
-		
+
 		System.out.println(s.getName() + "학생 정보가 제거되었습니다. ");
 	}
+
+	private void selectName() {
+		System.out.println("\n--- 학생 이름 검색 ---\n");
+
+		System.out.print("검색할 학생 이름 : ");
+		String name = sc.nextLine();
+
+		List<Student> list = service.selectName(name);
+		// 중복 이름이 있을 수 있으니 컬렉션의 arrayList로 불러오기
+
+		// 만약 검색 결과가 없을 경우
+		// list.size() : 리스트에 저장된 객체의 수를 의미!! 여기에 만약에 0개가 있으면 검색이 안됬다는 의미!(일치하는 이름이 없음)
+		// list.isEmpty() : 리스트에 저장된 객체가 없다면 true
+
+//		if( list.size() == 0 ) { // 저장된 객체의 수가 0개인 경우
+		if (list.isEmpty()) {
+			System.out.println(" [검색 결과가 없습니다.] ");
+
+		} else {
+			for (Student s : list)
+				System.out.println(s); // 컴파일러가 자동으로 toString을 추가해서 출력해줌
+		}
+	}
+
+	public void selectAddress() {
+		System.out.println("\n --- 학생 주소 검색 ---\n");
+
+		System.out.print("주소에 포함된 단어 입력 : ");
+		String input = sc.nextLine();
+
+		List<Student> list = service.selectAddress(input);
+
+		// 화면 출력을 3학년 5반 2번 홍길동 / 주소 : 서울시 중구
+		for (Student s : list) {
+			System.out.printf("%d학년 %d반 %d번 %s / 주소 : %s \n", s.getGrade(), s.getClassRoom(), s.getNumber(),
+					s.getName(), s.getAddress());
+		}
+
+	}
+
+	/**
+	 * 학년별 조회
+	 */
+	private void selectGrade() {
+		System.out.println("\n---학년별 조회 ---\n");
+
+		/*
+		 * 조회할 학년을 입력하세요 : 3
+		 * 
+		 * ****[조회한 학년이 있을 경우]**** 
+		 * -3학년 조회 결과 - 3학년 5반 2번 홍길동 3학년 2반 15번 송혜교
+		 * 
+		 * ****[조회할 학년이 없을 경우]**** 
+		 * [3학년 학생이 존재하지 않습니다.]
+		 * 
+		 */
+
+		System.out.print("조회할 학년을 입력하세요 : ");
+		int input = sc.nextInt();
+
+		List<Student> list = service.selectGrade(input);
+
+		if (list.isEmpty()) {
+			System.out.printf(" %d학년 학생이 존재하지 않습니다 ", input);
+
+		} else {
+			System.out.printf("%d학년 조회 결과\n", input);
+
+			for (Student s : list) {
+				System.out.printf("%d학년 %d반 %d번 %s\n", s.getGrade(), s.getClassRoom(), s.getNumber(), s.getName());
+			}
+
+		}
+
+	}
+	
+	private void selectGender() {
+	
+		System.out.println("\n --- 성별 조회 ---\n");
+		
+		while(true) { // 무한 반복
+			
+			System.out.print("조회할 성별을 입력하세요(M/F) : ");
+			char inputGender = sc.nextLine().toUpperCase().charAt(0);
+										// 대문자로된 char 데이터
+			if(inputGender == 'M' || inputGender == 'F') { // 정상 입력
+			
+				List<Student> list = service.selectGender(inputGender);
+			
+				if(list.isEmpty()) {
+					System.out.println("[ 검색 결과가 없습니다. ]");
+				} else {
+//					char gender = inputGender == 'M' ? '남' : '여';
+				
+				char gender;
+				if(inputGender =='M') gender = '남';
+				else				  gender = '여';
+				
+				System.out.printf("[%c학생 목록]\n", gender);
+				
+				for(Student s : list) {
+					System.out.printf("%d학년 %d반 %d번 %s\n", s.getGrade(), s.getClassRoom(), s.getNumber(), s.getName());
+				}
+			}
+			
+			break;  // 정상 입력 시 반복 종료
+			
+		}else { // 잘못 입력
+			System.out.println("[M 또는 F만 입력해주세요.]");
+			
+		}
+		
+	}
+		
+		/**조회할 성별을 입력하세요(M/F) : m / M / f / F 상관없이
+		 * 
+		 * [ M 또는 F만 입력 해주세요.]
+		 * 
+		 * 조회할 성별을 입력하세요(M/F) : (정상 입력이 될 때 까지 무한 반복)
+		 * 
+		 * *****정상 입력인 경우 ****
+		 * 
+		 * 조회할 성별을 입력하세요(M/F) : F
+		 * [여학생 목록]			(M 입력 시 [남학생 목록])
+		 * 2학년 7반 12번 김갑순(F)
+		 * 2학년 3반 9번 송혜교(F)
+		 * 
+		 * */
+		
+	
+		/*System.out.println("\n--- 성별 조회 --- \n");
+		char inputGender = 0;
+		List<Student> list = service.selectGender(inputGender);
+		
+		while(true) {
+			System.out.print("조회할 성별을 입력하세요(M/F) : ");
+			inputGender = sc.next().charAt(0);
+
+			
+			if(inputGender != 'F' && inputGender !='f' && inputGender !='m'&& inputGender != 'M') {
+				System.out.println("[ M 또는 F만 입력 해주세요.]");
+			}
+			else {
+				break;
+			}
+		
+		}
+		if(inputGender == 'f' || inputGender == 'F') {
+			for (Student f : list) {
+				System.out.printf("%d학년 %d반 %d번 %s", f.getGrade(), f.getClass(), f.getNumber(), f.getName());
+			}
+		}
+		if(inputGender == 'm'|| inputGender == 'M') {
+			for (Student m : list) {
+				System.out.printf("%d학년 %d반 %d번 %s", m.getGrade(), m.getClass(), m.getNumber(), m.getName());
+			}
+		}
+		*/
+		
+	}
+	
+	/**
+	 * 성적 순서 조회
+	 * */
+	private void sortScore() {
+		System.out.println("\n --- 성적 순서 조회 ---\n");
+		
+		List<Student> studentList = service.sortScore();
+		
+		service.sortScore();
+	}
+	
+	
+	
+
 }
